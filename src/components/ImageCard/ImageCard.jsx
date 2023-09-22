3659a684928a50ee438f71e4dad911ef1c0fe918
Sadique Jemeel Adekunle <sadiqadekunle1@gmail.com>
2023-09-22 01:09:40 +0100

    styles adjusted

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -4,7 +4,12 @@ import img2 from '../../Assets/Images/image-bg-light.jpg'
 import img3 from '../../Assets/Images/img-bg-dark.jpg'
 import img4 from '../../Assets/Images/image-bg-dark.jpg'
 import img5 from '../../Assets/Images/login_register.jpg'
-import img6 from '../../Assets/Images/image-bg-dark.jpg'
+import img6 from '../../Assets/Images/images4.jpg'
+import img7 from '../../Assets/Images/images2.jpeg'
+import img8 from '../../Assets/Images/images1.jpeg'
+import img9 from '../../Assets/Images/image3.jpeg'
+import img10 from '../../Assets/Images/images5.jpg'
+import img11 from '../../Assets/Images/images6.jpg'
 import './ImageCard.css'
 
 
@@ -17,10 +22,15 @@ const ImageCard = ({files}) => {
     const item4 = img4
     const item5 = img5
     const item6 = img6
+    const item7 = img7
+    const item8= img8
+    const item9 = img9
+    const item10 = img10
+    const item11= img11
   
   const dragItem = useRef();
   const dragOverItem = useRef();
-  const [list, setList] = useState([item1, item2, item3 , item4, item5,]);
+  const [list, setList] = useState([item1, item2, item3 , item4, item5, item6, item7, item8, item9, item10, item11]);
   const dragStart = (e, position) => {
     dragItem.current = position;
   };
